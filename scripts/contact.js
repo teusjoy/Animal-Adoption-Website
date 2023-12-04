2cf7c22cd51c23b3a3eb4a0f783bd265a26b7181
Fred <96602008+FredericGariepy@users.noreply.github.com>
2023-12-04 13:18:49 -0500

    Add files via upload

---
+++ scripts/contact.js
@@ -0,0 +1,25 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    alert('Thank you for your message, ' + name + '!');
+    
+});
+
+function validateEmail(email) {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
